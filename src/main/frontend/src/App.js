9fc33dbc3f2d091fefff0af9af6b93f420465c2d
scanacar <canacar26@gmail.com>
2021-03-29 23:38:59 +0300

    second commit

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,22 +4,7 @@ import SignupPage from './Components/SignUpPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Selamun aleyk.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
   );
 }
 

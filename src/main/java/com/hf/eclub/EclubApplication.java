34a5c87856f1c779f98a6fbff7ed05feb8ebc551
i-ates <i.ates@protonmail.com>
2021-03-29 16:19:42 +0300

    Initial Commit

---
+++ src/main/java/com/hf/eclub/EclubApplication.java
@@ -0,0 +1,13 @@
+package com.hf.eclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hf/eclub/EclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hf.eclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

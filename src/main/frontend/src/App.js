8808087e3b0d95a3f8f02b0fc0a42c15abd5f35b
mustafakollu <mustafakollu@hotmail.com>
2021-03-30 14:19:48 +0300

    Kanks manks ve de canks

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import SignupPage from './Components/SignUpPage'
-//mert
+//mert + mustafa
 function App() {
   return (
     <div className="App">

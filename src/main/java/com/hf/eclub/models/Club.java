562f4cf8409241b2649745a7833b1f8d32af1a9d
i-ates <i.ates@protonmail.com>
2021-04-05 14:21:50 +0300

    After signup auto navigation added

---
+++ src/main/java/com/hf/eclub/models/Club.java
@@ -0,0 +1,28 @@
+package com.hf.eclub.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Table(name = "club")
+
+public class Club {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(name="clubname")
+
+    private String clubName;
+
+    @Column(name="activeuser")
+
+    private int activeUser;
+
+    @Column(name="registereduser")
+
+    private int registeredUser;
+}
---
+++ src/main/java/com/hf/eclub/models/Question.java
@@ -0,0 +1,24 @@
+package com.hf.eclub.models;
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Table(name = "question")
+
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(name="question")
+
+    private String ques;
+
+    @Column(name="clubname")
+
+    private String clubName;
+
+}

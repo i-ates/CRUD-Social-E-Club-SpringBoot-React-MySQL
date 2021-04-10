ec72abe7563c89b72f2a5e4814400965577e0b17
i-ates <i.ates@protonmail.com>
2021-04-10 13:57:48 +0300

    submit function added

--- src/main/frontend/src/components/listQuestions.js
+++ src/main/frontend/src/components/listQuestions.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react'
 import UserService from "../services/user.service";
+import tap from "lodash/fp/tap";
+import flow from "lodash/fp/flow";
+import groupBy from "lodash/fp/groupBy";
+import AuthService from "../services/auth.service";
 
 class ListQuestions extends Component{
     constructor(props) {
@@ -18,6 +22,7 @@ class ListQuestions extends Component{
     //backendde yollucaz. Userid- clubid
     //
 
+
     componentDidMount() {
         UserService.getQuestion().then((res) =>{
             this.setState({questions: res.data});
@@ -28,24 +33,43 @@ class ListQuestions extends Component{
             "id":id,
             "answer":ans,
             "expectedAnswer":expected,
-            "clubIdd":clubId
+            "clubId":clubId
         }
         let prevanswer=this.state.answers.find(answer=>answer.id===id);
         if (prevanswer==null){
             this.setState({
                 answers: this.state.answers.concat([answer])
             })
-            console.log(id+" "+"yes clicked");
+            console.log(id+" "+ans+" clicked");
         }else{
            this.setState({
                answers:this.state.answers.map(el=>(el.id===id ? Object.assign({},el,answer):el))
            })
             console.log(id+" "+"answer updated");
         }
-
-
-
     }
+    submit(){
+        const currentUser = AuthService.getCurrentUser();
+        const map = require('lodash/fp/map').convert({ 'cap': false });
+        const result = flow(
+            groupBy('clubId'),
+            map((answers, clubId) => ({clubId, answers}))
+        )(this.state.answers)
+        let correctCount=0;
+        // console.log(result);
+        for (const [key,value] of result.entries()){
+            correctCount=0;
+            for (const [key1,value1] of value.answers.entries()){
+                if (value1.answer===value1.expectedAnswer){
+                    correctCount++;
+                }
+            }
+            if (correctCount/3 >0.5){
+                console.log(value.clubId+" will added"+" userid="+currentUser.id);
+            }
+        }
+    }
+
 
     render() {
         return (
@@ -66,10 +90,34 @@ class ListQuestions extends Component{
                                     <tr key ={question.id}>
                                         <td>{question.ques}</td>
                                         <td>
-                                            <button className="btn btn-outline-success"
-                                                    onClick={()=>this.getAnswer(question.id,"yes",question.clubId,question.answer)}>Yes</button>
-                                            <button className="btn btn-outline-warning"
-                                                    onClick={()=>this.getAnswer(question.id,"no",question.clubId,question.answer)}>No</button>
+                                            <div className="form-check form-check-inline">
+                                                <input className="form-check-input" type="checkbox" id="inlineCheckbox1"
+                                                       value="option1" onClick={()=>this.getAnswer(question.id,"yes",question.clubId,question.answer)} />
+                                                <label className="form-check-label"
+                                                       htmlFor="inlineCheckbox1">Yes</label>
+                                            </div>
+                                            <div className="form-check form-check-inline">
+                                                <input className="form-check-input" type="checkbox" id="inlineCheckbox2"
+                                                       value="option2" onClick={()=>this.getAnswer(question.id,"no",question.clubId,question.answer)}/>
+                                                <label className="form-check-label"
+                                                       htmlFor="inlineCheckbox2">No</label>
+
+                                                {/*<div className="form-check form-check-inline">*/}
+                                                {/*    <input className="form-check-input" type="radio"*/}
+                                                {/*           name="inlineRadioOptions" id="inlineRadio1"  value="option1"*/}
+                                                {/*           onClick={()=>this.getAnswer(question.id,"yes",question.clubId,question.answer)}/>*/}
+                                                {/*        <label className="form-check-label"*/}
+                                                {/*               htmlFor="inlineRadio1">Yes</label>*/}
+                                                {/*</div>*/}
+                                                {/*<div className="form-check form-check-inline">*/}
+                                                {/*    <input className="form-check-input" type="radio"*/}
+                                                {/*           name="inlineRadioOptions" id="inlineRadio2" value="option2"*/}
+                                                {/*           onClick={()=>this.getAnswer(question.id,"no",question.clubId,question.answer)}/>*/}
+                                                {/*        <label className="form-check-label"*/}
+                                                {/*               htmlFor="inlineRadio2">No</label>*/}
+                                                {/*</div>*/}
+                                            </div>
+
                                         </td>
                                     </tr>
                             )
@@ -77,7 +125,7 @@ class ListQuestions extends Component{
                         </tbody>
                     </table>
                 </div>
-                <button className="btn btn-primary" onClick={this.subm}>Submit</button>
+                <button className="btn btn-primary" onClick={()=>this.submit()}>Submit</button>
             </div>
         )
     }

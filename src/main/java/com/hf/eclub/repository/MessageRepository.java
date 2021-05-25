c3f5196e9bdbc0eb6ebecae3b544061281a3dee9
sec1911 <sencerkaya@protonmail.com>
2021-05-25 16:47:43 +0300

    Message delete operations added

--- src/main/java/com/hf/eclub/repository/MessageRepository.java
+++ src/main/java/com/hf/eclub/repository/MessageRepository.java
@@ -2,7 +2,11 @@ package com.hf.eclub.repository;
 
 import com.hf.eclub.models.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,5 +16,11 @@ public interface MessageRepository extends JpaRepository<Message, Long>{
 
     List<Message> findByClubId(Long clubId);
     List<Message> findByClubIdAndIsPrivate(Long clubId, Boolean isPrivate);
+    Message findById(long id);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Message as msg WHERE msg.clubId = :id")
+    void deleteAllMessagesByClubId(@Param("id") long id);
 
 }
